9d03891456d7b13ff76d1e2dccf8bb33d6ffe427
Amirev <128831376+Amirev@users.noreply.github.com>
2023-03-25 16:01:32 +0530

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -7,6 +7,7 @@ import os
 #import tensorflow_hub as hub
 import time ,sys
 from streamlit_embedcode import github_gist
+github_gist("https://gist.github.com/randyzwitch/be8c5e9fb5b8e7b046afebcac12e5087/")
 import urllib.request
 import urllib
 import moviepy.editor as moviepy
